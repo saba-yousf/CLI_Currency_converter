be00b4093d7cc8a5547bad037da27030029f53d5
Saba-yosuf <162787231+saba-yousf@users.noreply.github.com>
2024-06-27 21:52:21 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+
+const Currency: any = {
+
+    USD: 1,  //Base Currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+  
+let user_answer = await inquirer.prompt(
+  [  
+        {
+            name: "from",
+            message: "Enter from Currency",
+            type: "list",
+            choices: [ 'USD' , 'EUR' , 'GBP' , 'INR' , 'PKR' ]
+       },
+
+       {
+            name: "to",
+            message: "Enter to Currency",
+            type: "list",
+            choices: [ 'USD' , 'EUR' , 'GBP' , 'INR' , 'PKR' ]
+       },
+
+       {
+           name: "amount",
+           message: "Enter your Amount",
+           type: "number"
+       },
+   ]
+);
+
+
+let userfromCurrency = user_answer.from
+let usertoCurrency = user_answer.to
+let fromAmount = Currency[user_answer.from]; // exchange rate
+let toAmount = Currency[user_answer.to];  // exchange rate
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount  // base currancy is USD $
+let converteAmount = baseAmount * toAmount
+
+console.log(converteAmount);
+
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
